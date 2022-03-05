a6eee8ce9f8cc259cffdab03daf846280a3b979c
Paul Leydier <paul.leydier@compass-ft.com>
2022-03-05 21:54:27 +0100

    📝 docstring for exposed methods

--- src/heap.go
+++ src/heap.go
@@ -4,10 +4,14 @@ type MaxHeap[T Ordered] struct {
 	elements []T
 }
 
+// Insert inserts a value within the MaxHeap structure,
+// then Heapifies it from the bottom
 func (h *MaxHeap[T]) Insert(value T) {
 
 }
 
+// Extract returns the largest value from the MaxHeap structure (the one at the top),
+// removing it from the structure, and then Heapifies it from the top
 func (h *MaxHeap[T]) Extract() T {
 	return nil
 }
