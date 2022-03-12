9046de7618dc44e9202b37ade4a553f58681956d
Paul Leydier <paul.leydier@compass-ft.com>
2022-03-13 00:02:20 +0100

    ✨ MaxHeap Extract feature

--- src/heap.go
+++ src/heap.go
@@ -18,8 +18,9 @@ func (h *MaxHeap[T]) Insert(value T) {
 // Extract returns the largest value from the MaxHeap structure (the one at the top),
 // removing it from the structure, and then Heapifies it from the top
 func (h *MaxHeap[T]) Extract() T {
-	maxVal := h.elements[0]
-	h.elements = h.elements[1:]
+	maxVal := h.elements[0]                // take highest value
+	h.elements[0] = h.elements[h.length-1] // place the latest value in the top spot
+	h.elements = h.elements[:h.length-1]   // remove the last spot
 	h.length--
 	h.heapifyFromTop()
 	return maxVal
@@ -46,5 +47,30 @@ func (h *MaxHeap[T]) heapifyFromBottom() {
 }
 
 func (h *MaxHeap[T]) heapifyFromTop() {
+	currentNode, firstChild := 0, 1
+	for c := h.children(currentNode); len(c) > 0; {
+		largestChild := firstChild + ArgMax(c)
+		if h.elements[largestChild] > h.elements[currentNode] {
+			h.elements[largestChild], h.elements[currentNode] = h.elements[currentNode], h.elements[largestChild]
+			currentNode = largestChild
+		} else {
+			smallestChild := firstChild + ArgMin(c)
+			if h.elements[smallestChild] > h.elements[currentNode] {
+				h.elements[smallestChild], h.elements[currentNode] = h.elements[currentNode], h.elements[smallestChild]
+				currentNode = smallestChild
+			}
+		}
+		c, firstChild = h.children(currentNode), currentNode*2+1
+	}
+}
 
+func (h *MaxHeap[T]) children(idx int) []T {
+	potentialLastChild := idx*2 + 2
+	if h.length <= potentialLastChild {
+		if h.length <= potentialLastChild-1 {
+			return []T{}
+		}
+		return []T{h.elements[potentialLastChild-1]}
+	}
+	return []T{h.elements[potentialLastChild-1], h.elements[potentialLastChild]}
 }
